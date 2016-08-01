fd054ef5103b8fee0f3e196d6766f82f66e11ea1
marb <marb61a@gmail.com>
2016-08-01 10:32:44 +0000

    added barrel function

--- js/main.js
+++ js/main.js
@@ -110,16 +110,31 @@ var GameState = {
         
         if(this.cursors.left.isDown || this.player.customParams.isMovingLeft){
             this.player.body.velocity.x = -this.RUNNING_SPEED;
+            this.player.scale.setTo(1, 1);
+            this.player.play('walking');
         }
         
         else if(this.cursors.right.isDown || this.player.customParams.isMovingRight){
             this.player.body.velocity.x = this.RUNNING_SPEED;
+            this.player.scale.setTo(-1, 1);
+            this.player.play('walking');
+        }
+        
+        else {
+            this.player.animations.stop();
+            this.player.frame = 3;
         }
         
         if((this.cursors.up.isDown || this.player.customParams.mustJump) && this.player.body.touching.down) {
             this.player.body.velocity.y = -this.JUMPING_SPEED;
             this.player.customParams.mustJump = false;
         }
+        
+        this.barrels.forEach(function(element){
+            if(element.x < 10 && element.y > 600) {
+                element.kill();
+            }    
+        }, this);
     },
     
     createOnScreenControls : function(){
@@ -172,7 +187,32 @@ var GameState = {
         this.rightArrow.events.onInputOut.add(function(){
             this.player.customParams.isMovingRight = false;
         }, this);
-    }
+    },
+    
+    killPlayer : function(player, fire){
+        console.log('auch!');
+        game.state.start('GameState');    
+    },
+    
+    win : function(player, goal){
+        alert('you win!');
+        game.state.start('GameState');    
+    },
+    
+    createBarrel : function(){
+        // First barrel dead sprite
+        var barrel = this.barrels.getFirstExists(false);
+        
+        if(!barrel) {
+            barrel = this.barrels.create(0, 0, 'barrel');
+        }
+        
+        barrel.body.collideWorldBounds = true;
+        barrel.body.bounce.set(1, 0);
+        
+        barrel.reset(this.levelData.goal.x, this.levelData.goal.y);
+        barrel.body.velocity.x = this.levelData.barrelSpeed;
+        }
 };
 
 //Initialise the Phaser framework
